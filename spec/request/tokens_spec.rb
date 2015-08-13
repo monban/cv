141fac377b7a075ff04df019d4b7a6a49a311793
Ian Flynn <himitsu.monban@gmail.com>
2015-08-12 23:46:28 -0400

    don't show contact information without a valid token

--- spec/request/tokens_spec.rb
+++ spec/request/tokens_spec.rb
@@ -1,23 +1,41 @@
 require 'rails_helper'
 
-describe "token middleware", :type => :request do
+describe 'token middleware', :type => :request do
   let(:token) { FactoryGirl.create(:token) }
 
-  context "with a valid key" do
+  context 'with a valid key' do
     let(:key) { token.key  }
 
-    it "finds the correct token" do
+    it 'finds the correct token' do
       get '/', {token: key}
       expect(Token.current).to eq(token)
     end
+
+
+    context 'with a reference' do
+      let!(:reference) { FactoryGirl.create(:reference) }
+
+      it 'shows contact information' do
+        get '/', {token: key}
+        expect(response.body).to include(reference.telephone)
+      end
+    end
   end
 
-  context "when incorrect key" do
+  context 'when incorrect key' do
     let(:key) { token.key + 'foo' }
 
     it 'returns nil' do
       get '/', {token: key + 'foo'}
       expect(Token.current).to be_nil
+    end
+    context 'with a reference' do
+      let!(:reference) { FactoryGirl.create(:reference) }
+
+      it 'does not show contact information' do
+        get '/'
+        expect(response.body).to_not include(reference.telephone)
+      end
     end
 end
 
@@ -25,6 +43,15 @@ context 'when no key' do
     it 'returns nil' do
       get '/'
       expect(Token.current).to be_nil
+    end
+
+    context 'with a reference' do
+      let!(:reference) { FactoryGirl.create(:reference) }
+
+      it 'does not show contact information' do
+        get '/'
+        expect(response.body).to_not include(reference.telephone)
+      end
     end
   end
 end
