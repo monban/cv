63abf4078c6a0dfaa4649a45ffafb34cc6e7add7
Ian Flynn <himitsu.monban@gmail.com>
2015-08-12 22:49:28 -0400

    cleanup tests

--- spec/request/tokens_spec.rb
+++ spec/request/tokens_spec.rb
@@ -2,8 +2,29 @@ require 'rails_helper'
 
 describe "token middleware", :type => :request do
   let(:token) { FactoryGirl.create(:token) }
-  it "finds the correct token" do
-    get '/', {token: token.key}
-    expect(Token.current).to eq(token)
+
+  context "with a valid key" do
+    let(:key) { token.key  }
+
+    it "finds the correct token" do
+      get '/', {token: key}
+      expect(Token.current).to eq(token)
+    end
+  end
+
+  context "when incorrect key" do
+    let(:key) { token.key + 'foo' }
+
+    it 'returns nil' do
+      get '/', {token: key + 'foo'}
+      expect(Token.current).to be_nil
+    end
+end
+
+context 'when no key' do
+    it 'returns nil' do
+      get '/'
+      expect(Token.current).to be_nil
+    end
   end
 end
