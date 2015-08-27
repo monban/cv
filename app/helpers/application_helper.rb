911a43ab61ea6005c5b0f8c6c36d6f40871b8206
Ian Flynn <himitsu.monban@gmail.com>
2015-08-27 19:10:32 -0400

    added helper function to determine if admin logged in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def current_token
     Session.new(session).current_token
   end
+  def admin?
+    current_token ? current_token.admin? : false
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 describe ApplicationHelper do
-  context 'when token set in session' do
+  context 'with admin token set in session' do
+    let(:token) { FactoryGirl.create(:admin_token) }
+    before(:each) do
+      session['token_id'] = token.id
+    end
+
+    describe '#admin?' do
+      it 'returns true' do
+        expect(admin?).to be_truthy
+      end
+    end
+    
+  end
+  context 'when user token set in session' do
     let(:token) { FactoryGirl.create(:token) }
     before(:each) do
       session['token_id'] = token.id
@@ -12,12 +25,23 @@ describe ApplicationHelper do
         expect(current_token).to eq(token)
       end
     end
+
+    describe '#admin?' do
+      it 'returns false' do
+        expect(admin?).to be_falsey
+      end
+    end
   end
 
   context 'when no token set' do
     describe '#current_token' do
       it 'returns nil' do
         expect(current_token).to be_nil
+      end
+    end
+    describe '#admin?' do
+      it 'returns false' do
+        expect(admin?).to be_falsey
       end
     end
   end
