7a5b2f20d44d848922868d6fa625cdde1828a03c
Ian Flynn <himitsu.monban@gmail.com>
2015-08-27 20:28:47 -0400

    added edit and update controller methods

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,4 +1,21 @@
 class SectionsController < ApplicationController
   def index
+    @sections = Section.all
+  end
+
+  def edit
+    @section = Section.find params[:id] 
+  end
+
+  def update
+    @section = Section.find(params[:id])
+    @section.update(section_params)
+    redirect_to section_path(@section)
+  end
+
+  protected
+
+  def section_params
+    params.require(:section).permit(:title, :content)
   end
 end
--- spec/controllers/sections_controller_spec.rb
+++ spec/controllers/sections_controller_spec.rb
@@ -14,7 +14,29 @@ RSpec.describe SectionsController, type: :controller do
         get :index
         expect(response.status).to eq(200)
       end
+      context 'with a secton defined' do
+        let(:sections) { [FactoryGirl.create(:section)] }
+        it 'assigns all sections to @sections' do
+          get :index
+          expect(assigns(:sections)).to eq(sections)
+        end
+      end
     end
   end
-
+  describe '#edit' do
+    let(:section) { FactoryGirl.create(:section) }
+    it 'assigns the section to be edited to @section ' do
+      get :edit, id: section.id
+      expect(assigns(:section)).to eq(section)
+    end
+  end
+  
+  describe '#update' do
+    let(:section) { FactoryGirl.create(:section) }
+    it 'updates the object' do
+      expect {put(:update, id: section.id, section: {title: section.title.reverse})}.
+        to change{section.reload.title}.
+        from(section.title).to(section.title.reverse)
+    end
+  end
 end
