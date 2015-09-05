c953421ddcba731d0ea3a0ab0027a0e45d102cf3
Ian Flynn <himitsu.monban@gmail.com>
2015-09-05 14:08:07 -0400

    huge overhaul of sections controller, to be used as template for speccing other controllers

--- spec/controllers/sections_controller_spec.rb
+++ spec/controllers/sections_controller_spec.rb
@@ -2,43 +2,29 @@ require 'rails_helper'
 require 'controllers/support/admin_only'
 
 RSpec.describe SectionsController, type: :controller do
-  describe '#index' do
-    it_behaves_like "an admin controller"
-    context 'admin logged in' do
-      before(:each) do
-        RSpec::Mocks.configuration.verify_partial_doubles = false
-        allow(controller).to receive(:admin?).and_return(true) 
-      end
-      after(:each) do
-        RSpec::Mocks.configuration.verify_partial_doubles = true
-      end
-      it 'shows the page' do
-        get :index
-        expect(response.status).to eq(200)
-      end
-      context 'with a secton defined' do
-        let(:sections) { [FactoryGirl.create(:section)] }
-        it 'assigns all sections to @sections' do
-          get :index
-          expect(assigns(:sections)).to eq(sections)
-        end
-      end
+  it_behaves_like "an admin controller"
+  context 'admin logged in' do
+    let(:sections) { [FactoryGirl.create(:section)] }
+    before(:each) do
+      allow(subject).to receive(:admin?).and_return(true) 
     end
-  end
-  describe '#edit' do
-    let(:section) { FactoryGirl.create(:section) }
-    it 'assigns the section to be edited to @section ' do
-      get :edit, id: section.id
-      expect(assigns(:section)).to eq(section)
+
+    describe '#index' do
+      before { get :index }
+      it { expect(assigns(:sections)).to eq(sections) }
     end
-  end
-  
-  describe '#update' do
-    let(:section) { FactoryGirl.create(:section) }
-    it 'updates the object' do
-      expect {put(:update, id: section.id, section: {title: section.title.reverse})}.
-        to change{section.reload.title}.
-        from(section.title).to(section.title.reverse)
+    describe '#edit' do
+      let(:section) { sections.first }
+      before(:example) { get :edit, id: section.id }
+      it { expect(assigns(:section)).to eq(section) }
+    end
+    describe '#update' do
+      let(:section) { sections.first }
+      it 'updates the object' do
+        expect {put(:update, id: section.id, section: {title: section.title.reverse})}.
+          to change{section.reload.title}.
+          from(section.title).to(section.title.reverse)
+      end
     end
   end
 end
