a4c10477a3b8c431432ca25d26b925fe2b776832
Ian Flynn <himitsu.monban@gmail.com>
2015-09-07 04:17:59 -0400

    added new section / reference links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def new_section_link
+    if admin?
+      link_to 'New section', new_section_path
+    else
+      ''
+    end
+  end
+  def new_reference_link
+    if admin?
+      link_to 'New reference', new_reference_path
+    else
+      ''
+    end
+  end
 end
